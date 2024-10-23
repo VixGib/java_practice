b60697abf4a5b26dfa694adf1ab726534a1ae227
Vicki Gibbison <vickigibbison@hotmail.com>
2024-10-23 15:02:45 +0100

    CREATE: Task1 creating variables

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,11 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // initialising 2 string variables firstname and lastname and assigning values to them
+       String firstName = "Vicki";
+       String lastName = "Gibbison";
+       // initialising a string variable fullname and assigning the value of firstname plus lastname to create full name
+       String fullName = firstName + " " + lastName;
+        System.out.println("My name is " + fullName);
         }
     }
-}
