abe7242142140b7a77de0078d4f26eb76926d1f3
Vicki Gibbison <vickigibbison@hotmail.com>
2024-10-23 15:53:27 +0100

    CREATE: task3 using scanner to take and use a user input from the console

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+// import statement for java util scanner
+import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -9,26 +11,39 @@ public class Main {
 //       String fullName = firstName + " " + lastName;
 //        System.out.println("My name is " + fullName);
 
-        // initialising 2 integer variables and assigning them values
-        int num1 = 1, num2 = 2;
-        // printing out the total of num1 plus num2 by using  + in the parenthesis
-        System.out.println("The total of our 2 variables is " + (num1+num2));
-        // initialising the variable result and assigning the value num1 / num2
-        int result = num1 / num2;
-        System.out.println("The value of result is " + result);
+//        // initialising 2 integer variables and assigning them values
+//        int num1 = 1, num2 = 2;
+//        // printing out the total of num1 plus num2 by using  + in the parenthesis
+//        System.out.println("The total of our 2 variables is " + (num1+num2));
+//        // initialising the variable result and assigning the value num1 / num2
+//        int result = num1 / num2;
+//        System.out.println("The value of result is " + result);
+//
+//        // initialising 2 float variables and assigning them values
+//        float num3 = 1, num4 = 2;
+//        // printing out the total of num3 plus num4 by using  + in the parenthesis
+//        System.out.println("The total of our 2 variables is " + (num1+num2));
+//        // initialising the variable result2 and assigning the value of num3 / num4
+//        float result2 = num3 / num4;
+//        System.out.println("The value of result is " + result2);
+//        // initialising the integer variable num5 and assigning a value of 8
+//        int num5 = 8;
+//        // print the current value of num5 which is 8 and then add 1 to it
+//        System.out.println(num5++);
+//        // add one to the current value of num5 which is now 9 so it adds 1 then prints 10
+//        System.out.println(++num5);
 
-        // initialising 2 float variables and assigning them values
-        float num3 = 1, num4 = 2;
-        // printing out the total of num3 plus num4 by using  + in the parenthesis
-        System.out.println("The total of our 2 variables is " + (num1+num2));
-        // initialising the varaible result2 and assigning the value of num3 / num4
-        float result2 = num3 / num4;
-        System.out.println("The value of result is " + result2);
-        // initialising the integer variable num5 and assigning a value of 8
-        int num5 = 8;
-        // print the current value of num5 which is 8 and then add 1 to it
-        System.out.println(num5++);
-        // add one to the current value of num5 which is now 9 so it adds 1 then prints 10
-        System.out.println(++num5);
+        // create scanner object
+        Scanner myObj = new Scanner(System.in);
+        // get the input for the user
+        System.out.println("Enter your first name ");
+        // reading the console input and assigning it to firstName
+        String firstName = myObj.nextLine();
+        // get the input for the user
+        System.out.println("Enter your last name ");
+        // reading the console input and assigning it to lastName
+        String lastName = myObj.nextLine();
+        // printing out the full name by adding firstname and lastname together
+        System.out.println("Your full name is " + firstName + " " + lastName);
         }
     }
